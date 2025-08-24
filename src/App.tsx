e7d6ef885d5921ba707384d9968495ed2d1cb259
KeyZhai <1776978150@qq.com>
2025-08-25 02:25:57 +0800

    feat:add mini-calender component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-function App() {
+import Calendar, { type CalendarRef } from "./components/Calendat.tsx/Calendar"
+import { useRef, useState } from "react"
+
+function App() {  
+  const calendarRef = useRef<CalendarRef>(null);
+  const [state,setState] = useState(new Date());
   return (
     <div>
       <h1>Hello World</h1>
+      {/* 非受控组件 */}
+      {/* <Calendar ref={calendarRef} defaultValue={new Date()} onChange={(date) => {
+        alert("选中的日期为" + date)
+      }}/> */}
+      {/* 受控组件 */} 
+      <Calendar ref={calendarRef} defaultValue={state} onChange={(date) => {
+        setState(date);
+        alert("选中的日期为" + date)
+      }}/>
     </div>
   )
 }
---
+++ src/components/Calendat.tsx/Calendar.tsx
@@ -0,0 +1,89 @@
+import React,{useState,useImperativeHandle} from 'react';
+import styles from './Calendar.module.css';
+// import { useControllableValue } from 'ahooks';
+
+interface CalendarProps {
+  defaultValue?: Date;
+  onChange?: (date: Date) => void;
+  ref?: React.Ref<CalendarRef>;
+}
+
+export interface CalendarRef {
+  getDate: () => Date;
+  setDate:(date: Date) => void;
+}
+
+const Calendar = (props:CalendarProps) => {
+  const {defaultValue = new Date(),onChange,ref} = props
+  //非受控模式
+  const [date,setdate] = useState(defaultValue);
+  //受控模式
+  //useControllableValue  => 用于处理受控组件的状态
+  //会从props找value或者defaultValue作为初始值date初始值，找到onChange作为date改变后的回调
+  //并把更改后的value作为onchange参数传递回去
+  // const [date,setdate] = useControllableValue(props,{
+  //   defaultValue:new Date()
+  // })
+  const MonthName = ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"];
+  // useImperative Handle  => 用于控制ref向外暴露的方法
+  useImperativeHandle(ref,() => ({
+    getDate(){
+      return date;
+    },
+    setDate(date:Date){
+      setdate(date);
+    }
+  }))
+  const handlePrevMonth = () => {
+    setdate(new Date(date.getFullYear(), date.getMonth() - 1,1));
+  };
+
+  const handleNextMonth = () => {
+    setdate(new Date(date.getFullYear(), date.getMonth() + 1,1));
+  };
+
+  //render days
+  const renderDate = () => {
+    const countDays = new Date(date.getFullYear(),date.getMonth() + 1,0).getDate();
+    const startDay = new Date(date.getFullYear(),date.getMonth(),1).getDay();
+    const days = [];
+    for(let i = 0; i < startDay; i++) {
+      days.push(<div className={styles.empty} key={`empty-${i}`}></div>); 
+    }
+    for(let i = 1; i <= countDays; i++) {
+      const clickHandle = () => {
+        const curDate = new Date(date.getFullYear(), date.getMonth(), i);
+        setdate(curDate);
+        onChange?.(curDate);
+      }
+      if(i === date.getDate()){
+        days.push(<div className={`${styles.day} ${styles.selected}`} key={i} onClick={() => clickHandle()}>{i}</div>);
+      }else{
+        days.push(<div className={styles.day} key={i} onClick={() => clickHandle()}>{i}</div>);
+      }
+    }
+    return days;
+  }
+
+  return (
+    <div className={styles.calendar}>
+      <div className={styles.header}>
+        <button onClick={handlePrevMonth}>&lt;</button>
+        <div>{date.getFullYear()} 年 {MonthName[date.getMonth()]} </div>
+        <button onClick={handleNextMonth}>&gt;</button>
+      </div>
+      <div className={styles.days}>
+        <div className={styles.day}>日</div>
+        <div className={styles.day}>一</div>
+        <div className={styles.day}>二</div>
+        <div className={styles.day}>三</div>
+        <div className={styles.day}>四</div>
+        <div className={styles.day}>五</div>
+        <div className={styles.day}>六</div>
+       {renderDate()}
+      </div>
+    </div>
+  );
+}
+
+export default Calendar;
